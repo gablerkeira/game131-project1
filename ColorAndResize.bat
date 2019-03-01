ebe4f40a581b5c90e64f47dc0bea788dbcf17856
svc-student <svc-student@student.directory.jccc>
2019-02-28 19:22:40 -0600

    Color and resized

---
+++ ColorAndResize.bat
@@ -0,0 +1,37 @@
+@echo off 
+
+set fileName=%1
+
+set extension=%~x1
+
+echo %fileName%
+echo %extension%
+	
+IF /i %extension%==.jpg (
+	echo Conversion needed
+) else (
+	echo No conversion needed
+)
+
+set gray=gray_%1
+
+magick convert %1 -grayscale Rec709Luminance %gray%
+
+rem colors
+magick convert %gray% ( -clone 0 -fill red  -colorize 50 ) -compose multiply -composite red_%1
+magick convert %gray% ( -clone 0 -fill orange  -colorize 50 ) -compose multiply -composite orange_%1
+magick convert %gray% ( -clone 0 -fill yellow  -colorize 50 ) -compose multiply -composite yellow_%1
+magick convert %gray% ( -clone 0 -fill green  -colorize 50 ) -compose multiply -composite green_%1
+magick convert %gray% ( -clone 0 -fill blue  -colorize 50 ) -compose multiply -composite blue_%1
+magick convert %gray% ( -clone 0 -fill purple  -colorize 50 ) -compose multiply -composite purple_%1
+
+rem resize
+magick red_%1 -resize "256x256" -gravity center -extent 256x256 reduced_red.png
+magick orange_%1 -resize "256x256" -gravity center -extent 256x256 reduced_orange.png
+magick yellow_%1 -resize "256x256" -gravity center -extent 256x256 reduced_yellow.png
+magick green_%1 -resize "256x256" -gravity center -extent 256x256 reduced_green.png
+magick blue_%1 -resize "256x256" -gravity center -extent 256x256 reduced_blue.png
+magick purple_%1 -resize "256x256" -gravity center -extent 256x256 reduced_purple.png
+
+rem delete the gray image
+del /q %gray%
